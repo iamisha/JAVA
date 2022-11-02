e82d1ce851528b70d356e481f6afa71421db6aba
iamisha <isha.201710@ncit.edu.np>
2022-11-02 23:51:34 +0545

    takingInput_03 file created

--- day-2/takingInput_03.java
+++ day-2/takingInput_03.java
@@ -4,12 +4,20 @@ import java.util.Scanner;//importing the class Scanner that
 class takingInput_03 {
     public static void main(String[] args) {
         Scanner sn = new Scanner(System.in);
-        System.out.println("Enter the first value: ");
-        float a = sn.nextFloat();
-        System.out.println("Enter the second value: ");
-        float b = sn.nextFloat();
-        float sum = a + b;
-        System.out.println("The sum of the two numbers given is: ");
-        System.out.println(sum);
+        // System.out.println("Enter the first value: ");
+        // float a = sn.nextFloat();
+        // System.out.println("Enter the second value: ");
+        // float b = sn.nextFloat();
+        // float sum = a + b;
+        // System.out.println("The sum of the two numbers given is: ");
+        // System.out.println(sum);
+        // System.out.println("Enter any value to test hehe: ");
+        // boolean bl = sn.hasNextInt();
+        // System.out.println(bl);
+        System.out.println("Type a striing");
+        // String str = sn.next(); // it doesn't count the string after the space(counts
+        // single string)
+        String str = sn.nextLine();// it copies the whole string given by the user.
+        System.out.println(str);
     }
 }
