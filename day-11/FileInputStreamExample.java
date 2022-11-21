36cc791ae26003dae3723669c88437579713ae44
iamisha <isha.201710@ncit.edu.np>
2022-11-21 12:12:20 +0545

    FileInutStream added

---
+++ day-11/FileInputStreamExample.java
@@ -0,0 +1,48 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+/*
+ * class FileInputStreamExample {
+ * public static void main(String[] args) {
+ * try {
+ * FileInputStream fis = new FileInputStream("isha.txt");
+ * int data; // to store the ASCII dat
+ * while ((data = fis.read()) != -1) { // read the file till the end of file
+ * System.out.print((char) data); // converts the ascii data into char.
+ * 
+ * }
+ * 
+ * } catch (IOException e) {
+ * System.out.println(e.getMessage());
+ * }
+ * }
+ * }
+ */
+
+/*
+ * second method throwing the Exception
+ * class FileInputStreamExample {
+ * public static void main(String[] args) throws IOException {
+ * try {
+ * FileInputStream fis = new FileInputStream("file.txt");
+ * int d;
+ * while ((d = fis.read()) != -1) {
+ * System.out.print((char) d);
+ * }
+ * } catch (FileNotFoundException e) {
+ * System.out.println("creat a file at first");
+ * }
+ * }
+ * }
+ */
+class FileInputStreamExample {
+    public static void main(String[] args) throws IOException {
+        File file = new File("myfle.txt");
+        FileInputStream fis = new FileInputStream(file);
+        int d;
+        while ((d = fis.read()) != -1) {
+            System.out.print((char) d);
+        }
+    }
+}
--- day-11/readme.md
+++ day-11/readme.md
@@ -5,3 +5,16 @@
 ![CHEESE!](javaio.jpg)
 
 ![CHEESE!](java2nd.jpg)
+
+# FileInputStream and FileOutputStream class
+
+a. Used to read and write data in a file.
+b. Prefer this when primitive datatype values are to be written.
+c. For character oriented data, it's prefer to use FileReder and FileWriter class
+d. For Byte oriented data, it's prefer to use FileOutputStram class and FileInputStream.
+
+**e.Now let's see how we can red bytes from the file using the FileInputStream**
+
+Solution see in **["FileInputStreamExample.java"]**
+
+f. FileInputStream class doesn't make a new file for us if there is not present of the accessed file name.
